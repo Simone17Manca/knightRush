e743195f6c9ba03534634922c976dbefc2182ec0
Simone17Manca <116563176+Simone17Manca@users.noreply.github.com>
2024-06-04 09:57:05 +0200

    Initial commit

---
+++ scenes/slime.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+const speed = 60
+var direction = 1
+
+@onready var ray_cast_left = $RayCastLeft
+@onready var ray_cast_right = $RayCastRight
+
+func _process(delta):
+	if ray_cast_left.is_colliding():
+		direction=-1
+	if ray_cast_right.is_colliding():
+		direction=1
+		
+	position.x+=direction*speed*delta
+	
